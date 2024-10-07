8dc4a1e3fdca56913ce617cff69b54a1f2b88b58
Aayu1014 <aayushverma744@gmail.com>
2024-10-07 13:57:52 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -2,7 +2,6 @@ class Solution(object):
     def isValid(self, s):
         bracket_map = {')': '(', '}': '{', ']': '['}
         stack = []
-        
         for char in s:
             if char in bracket_map:
                 top_element = stack.pop() if stack else '#'
